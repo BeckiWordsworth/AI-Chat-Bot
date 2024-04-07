e30d16a28ef574b7f518636723113dd913ba5182
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-04-07 13:03:14 +0200

    Creating custom message forms

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useMultiChatLogic, MultiChatSocket, MultiChatWindow } from "react-chat-engine-advanced";
 import Header from "../customHeader";
+import StandardMessageForm from "../customMessageForms/StandardMessageForm.jsx";
 
 const Chat = () => {
   const chatProps = useMultiChatLogic(import.meta.env.VITE_PROJECT_ID, "testuser", "7654d");
@@ -8,7 +9,14 @@ const Chat = () => {
   return (
     <div style={{ flexBasis: "100%" }}>
       <MultiChatSocket {...chatProps} />
-      <MultiChatWindow {...chatProps} style={{ height: "100vh" }} renderChatHeader={(chat) => <Header chat={chat} />} />
+      <MultiChatWindow
+        {...chatProps}
+        style={{ height: "100vh" }}
+        renderChatHeader={(chat) => <Header chat={chat} />}
+        renderMessageForm={(props) => {
+          return <StandardMessageForm props={props} activeChat={chatProps.chat} />;
+        }}
+      />
     </div>
   );
 };
