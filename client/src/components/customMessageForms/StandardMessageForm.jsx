eb174e02a2e281a8eead48392e6072fcbc42057d
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-04-08 23:09:15 +0200

    Standard message form updates

--- client/src/components/customMessageForms/StandardMessageForm.jsx
+++ client/src/components/customMessageForms/StandardMessageForm.jsx
@@ -1,7 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
+import { XMarkIcon } from "@heroicons/react/24/solid";
 
 const StandardMessageForm = () => {
-  return <div>StandardMessageForm</div>;
+  const [message, setMessage] = useState("");
+  const [attachment, setAttachment] = useState("");
+  const [preview, setPreview] = useState("");
+
+  const handleChange = (e) => setMessage(e.target.value);
+
+  return (
+    <div className="message-form-container">
+      {preview && (
+        <div className="message-form-preview">
+          <img
+            alt="message-form-preview-image"
+            className="message-form-preview-image"
+            src={preview}
+            onLoad={() => URL.revokeObjectURL(preview)}
+          />
+          <XMarkIcon
+            className="message-from-icon-x"
+            onClick={() => {
+              setPreview("");
+              setAttachment("");
+            }}
+          />
+        </div>
+      )}
+      <div className="message-form">
+        <div className="message-from-input-container">
+          <input
+            className="message-form-input"
+            type="text"
+            value={message}
+            onChange={handleChange}
+            placeholder="Send a message..."
+          />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default StandardMessageForm;
