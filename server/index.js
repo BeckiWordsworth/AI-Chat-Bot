386c040d5f73bc7b0063e5965323c7c14f8ab8eb
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-04-11 22:04:51 +0200

    Server setup things

--- server/index.js
+++ server/index.js
@@ -16,3 +16,10 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+/* SERVER SETUP */
+
+const PORT = process.env.PORT || 9000;
+app.listen(PORT, () => {
+  console.log(`Example app listing on http//localhost:${PORT}`);
+});
