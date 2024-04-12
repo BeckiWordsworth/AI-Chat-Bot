4ba53cd9f93b55c8872749d831d50c72cb1251fb
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-04-12 23:11:41 +0200

    Adding openai and set up in server

--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ import cors from "cors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import helmet from "helmet";
+import { Configuration, OpenAIApi } from "openai";
 
 /* CONFIGURATION */
 
@@ -16,6 +17,13 @@ app.use(morgan("common"));
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
+
+/* OPEN AI CONFIGURATION */
+
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_API_KEY,
+});
+export const openai = new OpenAIApi(configuration);
 
 /* SERVER SETUP */
 
