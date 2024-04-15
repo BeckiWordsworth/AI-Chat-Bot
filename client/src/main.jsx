9ecb7f5fc1d53cd69035e045633749531d85c78c
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-04-15 23:02:02 +0200

    Setting up main with everything Redux

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,5 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
+import { Provider } from "react-redux";
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query";
+import { api } from "../src/state/api.js";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
