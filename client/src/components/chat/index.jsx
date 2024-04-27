e0619a363425ec22029019b2c0c3db5952dd7271
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-04-27 23:48:16 +0200

    USing Ai component conditionally

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useMultiChatLogic, MultiChatSocket, MultiChatWindow } from "react-chat-engine-advanced";
 import Header from "../customHeader";
 import StandardMessageForm from "../customMessageForms/StandardMessageForm.jsx";
+import Ai from "../customMessageForms/Ai";
 
 const Chat = () => {
   const chatProps = useMultiChatLogic(import.meta.env.VITE_PROJECT_ID, "testuser", "7654d");
@@ -14,6 +15,10 @@ const Chat = () => {
         style={{ height: "100vh" }}
         renderChatHeader={(chat) => <Header chat={chat} />}
         renderMessageForm={(props) => {
+          if (chatProps.chat?.title.startsWith("AiChat_")) {
+            return <Ai props={props} activeChat={chatProps.chat} />;
+          }
+
           return <StandardMessageForm props={props} activeChat={chatProps.chat} />;
         }}
       />
