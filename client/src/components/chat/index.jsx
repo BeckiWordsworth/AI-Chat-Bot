398d52f06e88dac03203cfdb4b651f35f7fce4aa
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-04-28 00:19:38 +0200

    AiCode component setup and connection

--- client/src/components/chat/index.jsx
+++ client/src/components/chat/index.jsx
@@ -3,6 +3,7 @@ import { useMultiChatLogic, MultiChatSocket, MultiChatWindow } from "react-chat-
 import Header from "../customHeader";
 import StandardMessageForm from "../customMessageForms/StandardMessageForm.jsx";
 import Ai from "../customMessageForms/Ai";
+import AiCode from "../customMessageForms/AiCode";
 
 const Chat = () => {
   const chatProps = useMultiChatLogic(import.meta.env.VITE_PROJECT_ID, "testuser", "7654d");
@@ -17,6 +18,10 @@ const Chat = () => {
         renderMessageForm={(props) => {
           if (chatProps.chat?.title.startsWith("AiChat_")) {
             return <Ai props={props} activeChat={chatProps.chat} />;
+          }
+
+          if (chatProps.chat?.title.startsWith("AiCode_")) {
+            return <AiCode props={props} activeChat={chatProps.chat} />;
           }
 
           return <StandardMessageForm props={props} activeChat={chatProps.chat} />;
