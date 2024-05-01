6b718a06173e4b2bd475899206bddbdf4e272486
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-05-01 14:14:48 +0200

    Updating routes to take into account authentication

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Chat from "./components/chat";
+import Login from "./components/login";
 
 function App() {
   const [user, setUser] = useState(null);
@@ -11,6 +12,10 @@ function App() {
     <div className="app">
       <BrowserRouter>
         <Routes>
+          <Route
+            path="/"
+            element={isAuth ? <Navigate to="/chat" /> : <Login setUser={setUser} setSecret={setSecret} />}
+          />
           <Route path="/chat" element={isAuth ? <Chat user={user} secret={secret} /> : <Navigate to="/" />} />
         </Routes>
       </BrowserRouter>
