bb4688c873701949d9453bc7abb98a72508c6457
Becki Wordsworth <63713673+BeckiWordsworth@users.noreply.github.com>
2024-05-01 14:21:58 +0200

    Beginning of login/signup page

--- client/src/components/login/index.jsx
+++ client/src/components/login/index.jsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { usePostLoginMutation, usePostSignUpMutation } from "@/state/api";
 
 const index = () => {
-  return <div>index</div>;
+  const [isRegister, setRegister] = useState(false);
+  const [username, setUserName] = useState("");
+  const [password, setPassword] = useState("");
+  const [triggerLogin] = usePostLoginMutation();
+  const [triggerSignUp] = usePostSignUpMutation();
+
+  return <div>Login</div>;
 };
 
 export default index;
